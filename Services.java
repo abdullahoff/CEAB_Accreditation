a564bea9ca27070c7ef704cb0fb064b218ab245f
abdullah abdullah <abdullah2004@live.ca>
2021-05-13 21:56:08 -0400

    services

--- Services.java
+++ Services.java
@@ -1,3 +1,29 @@
-public class Services {
+/**
+* Author: S. Smith
+* Revised: Mar 9, 2021
+* 
+* Description: ...
+*/
+
+package src;
+
+/**
+* @brief ...
+*/
+
+public class Services
+{
+    public static double[] normal(double[] v)
+    {
+        double[] nv = new double[v.length];
+        double total = 0.0;
+        for (int i=0; i<v.length; i++) {
+            total = total + v[i];
+        }
+        for (int i=0; i<v.length; i++) {
+            nv[i] = v[i]*(1.0/total);
+        }
+        return nv;
+    }
     
 }
